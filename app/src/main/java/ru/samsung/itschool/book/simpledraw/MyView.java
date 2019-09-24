380eb4a2cd6e61ec3b2a6b456ada91903c44be27
Artem <artemmakarevich1997@gmail.com>
2019-09-24 17:09:14 +0300

    Moving point

--- app/src/main/java/ru/samsung/itschool/book/simpledraw/MyView.java
+++ app/src/main/java/ru/samsung/itschool/book/simpledraw/MyView.java
@@ -2,6 +2,7 @@ package ru.samsung.itschool.book.simpledraw;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.view.View;
 
 import task.Task;
@@ -12,6 +13,10 @@ public class MyView extends View {
         super(context);
     }
 
+    Paint paint = new Paint ();
+    float x = 0;
+    long lastTime = System.currentTimeMillis();
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -20,5 +25,12 @@ public class MyView extends View {
 
         //Здесь располагаются команды рисования
         ///...
+
+        canvas.drawCircle(x, 300,20,paint);
+        long nowTime = System.currentTimeMillis();
+        x+=0.001f*(nowTime - lastTime);
+        lastTime=nowTime;
+
+        invalidate();// Ставим задачу на перерисовку
     }
 }
